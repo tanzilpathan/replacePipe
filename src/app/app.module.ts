950029f81572ff4e4cdeec0954d81323403f4957
TanzilVSS <tanzil.pathan@vishleshan.net>
2023-05-02 19:34:08 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReplacePipe } from 'src/replace.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ReplacePipe
   ],
   imports: [
     BrowserModule
